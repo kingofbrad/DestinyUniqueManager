40eea6cf8ae25062301a64e57229f14ef5d2da5c
Bradlee King <kingofbrad@gmail.com>
2024-02-06 18:26:28 +0000

    Initial Commit

---
+++ Destiny Unique Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Destiny Unique Manager
+//
+//  Created by Bradlee King on 06/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Destiny Unique Manager/Destiny_Unique_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Destiny_Unique_ManagerApp.swift
+//  Destiny Unique Manager
+//
+//  Created by Bradlee King on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Destiny_Unique_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
